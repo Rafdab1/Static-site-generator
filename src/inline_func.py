a4674877da183ee3bf6fa02282c0b6c417b6cb89
Rafdab1 <piter12029@gmail.com>
2024-07-26 23:51:34 +0200

    Added new function: Split_nodes_delimiter

---
+++ src/inline_func.py
@@ -0,0 +1,19 @@
+from textnode import (TextNode,text_type_bold,text_type_code,text_type_italic,text_type_text)
+
+def split_nodes_delimiter(old_nodes, delimiter, text_type):
+    new_nodes = []
+    for node in old_nodes:
+        if node.text_type != text_type_text:
+            new_nodes.append(node)
+            continue
+        sub_strings = node.text.split(delimiter)
+        if len(sub_strings) % 2 == 0:
+            raise Exception(f"Canot find closing delimeter {node}")
+        for i in range (len(sub_strings)):
+            if sub_strings[i] == "":
+                continue
+            if i % 2 == 0:
+                new_nodes.append(TextNode(sub_strings[i], text_type_text))
+            else:
+                new_nodes.append(TextNode(sub_strings[i], text_type))
+    return new_nodes
