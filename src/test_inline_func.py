b6ee24c210bf71816acf257525ad3c79638a0dbc
Rafdab1 <piter12029@gmail.com>
2024-07-27 12:09:54 +0200

    Added new test for test_inline_func.py

--- src/test_inline_func.py
+++ src/test_inline_func.py
@@ -9,3 +9,7 @@ class test_inline_functions(unittest.TestCase):
         self.assertEqual(split_nodes_delimiter([test_node],"**",text_type_bold),[TextNode("This is text with a ", "text") ,
                                                                                  TextNode("bolded phrase", "bold"),
                                                                                  TextNode(" in the middle", "text")])
+        
+    def test_split_nodes_delimiter_only_code_type(self):
+        node = TextNode("`code block``Only code`", text_type_text)
+        self.assertEqual(split_nodes_delimiter([node],"`",text_type_code),[ TextNode("code block", text_type_code), TextNode("Only code", text_type_code)])
